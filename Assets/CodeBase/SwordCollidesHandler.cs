f60b0a64f536c5bdfb95d88332882aa8bd59af0c
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-06-19 22:05:05 +0400

    animations, pickaxe, sword

---
+++ Assets/CodeBase/SwordCollidesHandler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SwordCollidesHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Enemy enemy))
+        {
+            Destroy(enemy.gameObject);
+        }
+    }
+}
