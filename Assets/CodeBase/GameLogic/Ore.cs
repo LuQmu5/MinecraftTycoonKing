e6596942e4b0fef1b685dc372b3ec7bdab0198e7
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-07-01 19:19:23 +0400

    Ores

---
+++ Assets/CodeBase/GameLogic/Ore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Ore : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out Pickaxe pickaxe))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
