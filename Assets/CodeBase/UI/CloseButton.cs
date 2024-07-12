3ddb19389044b893e931ac4a70c0642b0c9bbddf
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-07-12 16:48:23 +0400

    UI

---
+++ Assets/CodeBase/UI/CloseButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CloseButton : ButtonClickHandler
+{
+    [SerializeField] private GameObject _closableObject;
+
+    protected override void OnButtonClicked()
+    {
+        _closableObject.gameObject.SetActive(false);
+    }
+}
